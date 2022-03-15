8e30f36a4112b97bb1d26c5ef33ec08414befbc7
alexsukhotckii <alex.al11212123@mmmaail.gmail>
2022-03-15 20:45:01 +0300

    Message storage + Admin message deletion

--- pages/api/delete.js
+++ pages/api/delete.js
@@ -4,14 +4,18 @@ const uri =
 
 export default async function handler(req, res) {
     const client = new MongoClient(uri);
+    const { query } = req;
+
     try {
+        if (!query["sender"] || !query["password"] || query["password"] != process.env.PASSWORD) throw new Error("Invalid query");
+
         await client.connect();
         const database = client.db('MessangerDB');
-        const messages = await database.collection('Messages').find({}).toArray();
+        const messages = await database.collection('Messages').deleteMany({sender: query["sender"]});
         res.status(200).json({ messages })
     } catch (e) {
         console.log(e)
-        res.status(500).json({ messages: [] })
+        res.status(500).json({ error: true })
     } finally {
         await client.close();
     }
--- pages/index.js
+++ pages/index.js
@@ -10,49 +10,55 @@ import { useRouter } from 'next/router'
 const fetcher = (...args) => fetch(...args).then(res => res.json()).then((data) => {
   let chat = []
   data.messages.forEach(msg => {
-    chat.push(<p><span className={styles.author}>{msg.sender}</span>: <div className={styles.messageblue}>{msg.content}</div></p>)
+    chat.push(<div key={msg._id}><span className={styles.author}>{msg.sender}</span>: <div className={styles.messageblue}>{msg.content}</div></div>)
   })
   return chat
 })
 
 export default function Home() {
   const [message, setMessage] = useState("")
-  const [username, setUsername] = useState("lolxdlolidkthisisaneasteregg")
+  const [username, setUsername] = useState("")
+  const [cookie, setCookie] = useCookies(["username", "message"])
   const { data, ferror } = useSWR('/api/messages', fetcher, { refreshInterval: 100 })
+  const router = useRouter()
   const messagesEnd = useRef(null)
+
+  const MAX_AGE = 60 * 60 * (Math.random() * 999)
   let error = ""
   let canSend = true;
-  const MAX_AGE = 60 * 60 * 999
-  const [cookie, setCookie] = useCookies(["username"], ["message"])
-  const [usernameCookie, setUsernameCookie] = useState(cookie.username)
-  const rter = useRouter()
 
-  if (username == 'lolxdlolidkthisisaneasteregg') {
-    setUsername(usernameCookie)
-  }
-  if (username) {
-    if (String(username).replace(/ /gi, '').replace(/​/gi, '').replace(/ /gi, '') == "") {
-      error = 'Отсутствует имя пользователя. Введите его в поле "Имя пользователя".'
-      canSend = false
+  useEffect(() => {
+    if (username == '' && Boolean(cookie.username)) {
+      setUsername(cookie.username)
     }
-    if (String(username).length > 16) {
-      error = 'Имя пользователя слишком большое, оно должно быть не больше 16 символов.'
-      canSend = false
+    if (message == '' && Boolean(cookie.message)) {
+      setMessage(cookie.message)
     }
-  }
-  if (username == "") {
+    messagesEnd.current.scrollIntoView({ behavior: "smooth" })
+  }, [message, cookie.message, username, cookie.username])
+
+  if (!Boolean(username) || String(username).replace(/ /gi, '').replace(/​/gi, '').replace(/ /gi, '') == "") {
     error = 'Отсутствует имя пользователя. Введите его в поле "Имя пользователя".'
-      canSend = false
-  }
-  if (message.replace(/ /gi, '').replace(/​/gi, '').replace(/ /gi, '') == "") {
     canSend = false
   }
-  useEffect(() =>
-    messagesEnd.current.scrollIntoView({ behavior: "smooth" })
-  )
+  if (Boolean(username) && String(username).length > 16) {
+    error = 'Имя пользователя слишком большое, оно должно быть не больше 16 символов.'
+    canSend = false
+  }
+
+  if (!Boolean(message) || String(message).replace(/ /gi, '').replace(/​/gi, '').replace(/ /gi, '') == "") {
+    canSend = false
+  }
+
+  if (!Boolean(error) && Boolean(ferror)) {
+    error = "Не удалось получить сообщения"
+    canSend = false
+    console.log(ferror)
+  }
+
   return (
     <div className={styles.container}>
-      <button onClick={() => rter.push("/easter_egg")}>Пасхалка)</button>
+      <button onClick={() => router.push("/easter_egg")}>Пасхалка)</button>
       <Head>
         <title>54degree5 Messenger</title>
         <meta name="description" content="54degree5 — это безинтернетный, полностью и анонимный мессенджер!" />
@@ -60,49 +66,55 @@ export default function Home() {
       </Head>
       <div>
         <center><h1>54degree5 Messenger — это <span className={styles.bluecol}>безинтернетный</span>, полностью <span className={styles.bluecol}>анонимный</span> мессенджер!</h1></center>
-        <center className><h3>Сайт сделан <span className={styles.bluecollink}><Link href={"https://github.com/54degree5"}>
+        <center><h3>Сайт сделан <span className={styles.bluecollink}><Link href={"https://github.com/54degree5"}>
           <a>
             Сухоцким Александром
           </a>
         </Link></span></h3></center>
         <div className={styles.messagebox}>{data}<div ref={messagesEnd} /></div>
         <main className={styles.main}>
-          <form className={styles.form} onSubmit={(e) => {
-            setMessage("")
-            e.preventDefault()
-            fetch('/api/send', {
-              method: 'POST',
-              headers: {
-                'Content-Type': 'application/json'
-              },
-              body: JSON.stringify({
-                sender: username,
-                content: message
-              })
-            });
-          }}>
-            <div className={styles.sendcontainer}>
+          <div className={styles.form}>
+            <form className={styles.sendcontainer} onSubmit={(e) => {
+              e.preventDefault()
+              setMessage("")
+              setCookie("message", "", {
+                path: "/",
+                maxAge: MAX_AGE,
+                sameSite: true,
+              });
+              fetch('/api/send', {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                  sender: username,
+                  content: message
+                })
+              });
+            }}>
               <input className={styles.username} onChange={(e) => {
-                setUsernameCookie(e.target.value)
-                console.log(usernameCookie)
-                try {
-                  setCookie("username", e.target.value, {
-                    path: "/",
-                    maxAge: MAX_AGE,
-                    sameSite: true,
-                  })
-                } catch (err) {
-                  console.log(err)
-                }
+                setCookie("username", e.target.value, {
+                  path: "/",
+                  maxAge: MAX_AGE,
+                  sameSite: true,
+                })
                 setUsername(e.target.value)
-              }} placeholder="Имя пользователя" value={usernameCookie} />
-              <input className={styles.msgbox} onChange={(e) => { setMessage(e.target.value) }} value={message} placeholder="Сообщение" />
+              }} placeholder="Имя пользователя" value={username} />
+              <input className={styles.msgbox} onChange={(e) => {
+                setMessage(e.target.value);
+                setCookie("message", e.target.value, {
+                  path: "/",
+                  maxAge: MAX_AGE,
+                  sameSite: true,
+                });
+              }} value={message} placeholder="Сообщение" />
               <button className={styles.send} type="submit" disabled={!canSend}>
                 {'>'}
               </button>
-            </div>
+            </form>
             {error != '' && <p className={styles.error}>{error}</p>}
-          </form>
+          </div>
         </main>
       </div>
     </div>
